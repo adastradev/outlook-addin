05a1c0d55b618b41b462b27ec3c7a4b67edf0664
Kyle Rowan <krowan@aais.com>
2020-01-07 15:40:16 -0600

    Fix promise issue

--- webpack.config.js
+++ webpack.config.js
@@ -25,100 +25,96 @@ module.exports = (env, argv) => {
       } else {
         console.log(`Retrieved S3 object: ${data.Body.toString()}`);
         config = JSON.parse(data.Body.toString());
+      
+        return config.tenants.map((tenant) => {   
+          return {
+            name: tenant.instance,
+            devtool: 'source-map',
+            entry: {
+              vendor: [
+                'react',
+                'react-dom',
+                'core-js',
+                'office-ui-fabric-react'
+              ],
+              taskpane: [
+                  'react-hot-loader/patch',
+                  './src/taskpane/index.tsx',
+              ],
+            },
+              resolve: {
+              extensions: ['.ts', '.tsx', '.html', '.js'],
+            },
+            output: {
+              path: path.resolve(__dirname, `dist/${tenant.addinId}`)
+            },
+            module: {
+              rules: [
+                {
+                  test: /\.tsx?$/,
+                  use: [
+                    'react-hot-loader/webpack',
+                    'ts-loader'
+                  ],
+                  exclude: /node_modules/
+                },
+                {
+                  test: /\.css$/,
+                  use: ['style-loader', 'css-loader']
+                },
+                {
+                  test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                  use: {
+                    loader: 'file-loader',
+                    query: {
+                      name: 'assets/[name].[ext]'
+                    }
+                  }  
+                }   
+              ]
+            },
+            plugins: [
+              new CleanWebpackPlugin(),
+              new webpack.DefinePlugin({
+                __ADDIN_ID__: JSON.stringify(tenant.addinId),
+                __API_BASE_PATH__: JSON.stringify(tenant.bridgeURL),
+                __SCHEDULE_BASE_PATH__: JSON.stringify(config.baseScheduleURL)
+              }),
+              new CopyWebpackPlugin([{ 
+                to: 'taskpane.css', 
+                from: './src/taskpane/taskpane.css' 
+              }]),
+              new ExtractTextPlugin('[name].[hash].css'),
+              new HtmlWebpackPlugin({ 
+                filename: 'taskpane.html', 
+                template: './src/taskpane/taskpane.html', 
+                chunks: ['taskpane', 'vendor', 'polyfills'] 
+              }),
+              new HtmlWebpackPlugin({
+                filename: 'manifest.xml',
+                template: './manifest-template.xml',
+                addinId: tenant.addinId,
+                addinURL: `${config.baseURL}/${tenant.addinId}/taskpane.html`,
+              }),
+              new CopyWebpackPlugin([{ 
+                to: 'assets', 
+                from: './assets', 
+                ignore: ['*.scss'] 
+              }]),
+              new webpack.ProvidePlugin({
+                Promise: ["es6-promise", "Promise"]
+              })
+            ],
+            devServer: {
+              headers: {
+                "Access-Control-Allow-Origin": "*"
+              },      
+              https: false,
+              port: process.env.npm_package_config_dev_server_port || 3000
+            }
+          };
+        });      
       }
     }
   );
-  
-  if(config.tenants !== undefined) {
-    console.log(`Retrieved configuration: ${JSON.stringify(config)}`);
-  }
-
-  return config.tenants.map((tenant) => {   
-    return {
-      name: tenant.instance,
-      devtool: 'source-map',
-      entry: {
-        vendor: [
-          'react',
-          'react-dom',
-          'core-js',
-          'office-ui-fabric-react'
-        ],
-        taskpane: [
-            'react-hot-loader/patch',
-            './src/taskpane/index.tsx',
-        ],
-      },
-        resolve: {
-        extensions: ['.ts', '.tsx', '.html', '.js'],
-      },
-      output: {
-        path: path.resolve(__dirname, `dist/${tenant.addinId}`)
-      },
-      module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: [
-              'react-hot-loader/webpack',
-              'ts-loader'
-            ],
-            exclude: /node_modules/
-          },
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-          },
-          {
-            test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-            use: {
-              loader: 'file-loader',
-              query: {
-                name: 'assets/[name].[ext]'
-              }
-            }  
-          }   
-        ]
-      },
-      plugins: [
-        new CleanWebpackPlugin(),
-        new webpack.DefinePlugin({
-          __ADDIN_ID__: JSON.stringify(tenant.addinId),
-          __API_BASE_PATH__: JSON.stringify(tenant.bridgeURL),
-          __SCHEDULE_BASE_PATH__: JSON.stringify(config.baseScheduleURL)
-        }),
-        new CopyWebpackPlugin([{ 
-          to: 'taskpane.css', 
-          from: './src/taskpane/taskpane.css' 
-        }]),
-        new ExtractTextPlugin('[name].[hash].css'),
-        new HtmlWebpackPlugin({ 
-          filename: 'taskpane.html', 
-          template: './src/taskpane/taskpane.html', 
-          chunks: ['taskpane', 'vendor', 'polyfills'] 
-        }),
-        new HtmlWebpackPlugin({
-          filename: 'manifest.xml',
-          template: './manifest-template.xml',
-          addinId: tenant.addinId,
-          addinURL: `${config.baseURL}/${tenant.addinId}/taskpane.html`,
-        }),
-        new CopyWebpackPlugin([{ 
-          to: 'assets', 
-          from: './assets', 
-          ignore: ['*.scss'] 
-        }]),
-        new webpack.ProvidePlugin({
-          Promise: ["es6-promise", "Promise"]
-        })
-      ],
-      devServer: {
-        headers: {
-          "Access-Control-Allow-Origin": "*"
-        },      
-        https: false,
-        port: process.env.npm_package_config_dev_server_port || 3000
-      }
-    };
-  });
 }
